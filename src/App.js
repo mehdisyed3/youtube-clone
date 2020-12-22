a868ccef17a2e0a4c61ba22ff009a89e75c07980
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-21 21:40:50 -0500

    cleaned up  firebase- tools installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>hello</h1>
+      <h1></h1>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCqMyROln9xn38eQ7eVpnhrMXc6uhgPbos",
+  authDomain: "clone-82d7a.firebaseapp.com",
+  projectId: "clone-82d7a",
+  storageBucket: "clone-82d7a.appspot.com",
+  messagingSenderId: "629151335406",
+  appId: "1:629151335406:web:868a1a8ccd7748b17684ff"
+};
