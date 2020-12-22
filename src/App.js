a25ef8a27a7dbc750635cef68a7ead31fab71f0b
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-21 21:59:45 -0500

    material ui install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './App.css';
+import Header from './Header';
 
+28.30
 function App() {
   return (
     <div className="app">
-      <h1></h1>
+      <Header />
+      <h1>HYEkk</h1>
     </div>
   );
 }
