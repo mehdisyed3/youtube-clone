a65bdd21e6f758ccaba857f0f7dee453bb3650cf
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-22 13:24:30 -0500

    started working on Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css';
 import Header from './Header';
+import RecommendedVideos from './RecommendedVideos';
+import Sidebar from './Sidebar';
 
-// 28.30
+
+// 1.15
 function App() {
   return (
     <div className="app">
       <Header />
+      <div className='app__page'>
+        <Sidebar />
+        <RecommendedVideos />
+
+      </div>
 
     </div>
   );
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './RecommendedVideos.css'
+function RecommendedVideos() {
+  return (
+    <div className='recommendedVideos'>
+      <h1>Recommended</h1>
+    </div>
+  )
+}
+
+export default RecommendedVideos
