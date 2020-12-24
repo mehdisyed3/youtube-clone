d517ca789c5b48f35e6df396bd4eda00cf7a471c
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-23 20:13:34 -0500

    working on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarRow from './SidebarRow'
+import HomeIcon from '@material-ui/icons/Home'
+import WhatshotIcon from '@material-ui/icons/Whatshot'
+import SubscriptionIcon from '@material-ui/icons/Subscriptions'
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-      Sidebar
+      <SidebarRow Icon={HomeIcon } title='Home' />
+      <SidebarRow Icon={WhatshotIcon } title='Trending' />
+      <SidebarRow Icon={SubscriptionIcon } title='Subscription' />
+      {/* <SidebarRow title='' /> */}
+      
     </div>
   )
 }
