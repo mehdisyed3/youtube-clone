f1712a821f3f4beb6c2627a63dd7b9b2bc6c7356
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-28 19:41:22 -0500

    added VideoCArd component to Recommended Videos.js

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -48,6 +48,36 @@ function RecommendedVideos() {
           views='2.3M Views'
           channelImage='https://avatars1.githubusercontent.com/u/21147250?s=460&u=2e46b2ce19e4bcb38b64392b265d960f77cbc43a&v=4' />
 
+<VideoCard
+          image='https://picsum.photos/id/237/200'
+          title="Welcome to the "
+          channel="Syed Mehdi"
+          timestamp='3 days ago'
+          views='2.3M Views'
+          channelImage='https://avatars1.githubusercontent.com/
+        u/21147250?s=460&
+        u=2e46b2ce19e4bcb38b64392b265d960f77cbc43a&v=4'
+        />
+        <VideoCard
+          image='https://picsum.photos/id/237/200'
+          title="Welcome to the "
+          channel="Syed Mehdi"
+          timestamp='3 days ago'
+          views='2.3M Views'
+          channelImage='https://avatars1.githubusercontent.com/
+        u/21147250?s=460&
+        u=2e46b2ce19e4bcb38b64392b265d960f77cbc43a&v=4'
+        />
+        <VideoCard
+          image='https://picsum.photos/id/237/200'
+          title="Welcome to the "
+          channel="Syed Mehdi"
+          timestamp='3 days ago'
+          views='2.3M Views'
+          channelImage='https://avatars1.githubusercontent.com/
+        u/21147250?s=460&
+        u=2e46b2ce19e4bcb38b64392b265d960f77cbc43a&v=4'
+        />
       </div>
 
 
