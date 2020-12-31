3d1a4d64fb04a19fd2517f6468411050787e744b
Syed Mehdi <mehdisyed3@gmail.com>
2020-12-30 20:44:20 -0500

    VideoStream.js is now working, next step is to style the compponent

--- src/VideoStream.js
+++ src/VideoStream.js
@@ -3,15 +3,13 @@ import { useParams } from "react-router"
 
 function VideoStream() {
   const [str,setstr] = useState('')
-   const { searchTerm } = useParams()
-   console.log("@@@@",searchTerm)
-  // setstr(id)
-  // console.log("IDIDIDI",str)
+   const { id } = useParams()
+   console.log("@@@@",id)
 
   return (
     <div>
       <h1>Widi</h1>
-      {/* <iframe className="videoCard__thumbnail" src={`https://www.youtube.com/embed/${id}`} frameBorder="0" allowFullScreen></iframe>  */}
+      <iframe className="videoCard__thumbnail" src={`https://www.youtube.com/embed/${id}`} frameBorder="0" allowFullScreen></iframe> 
     </div>
   )
 }
